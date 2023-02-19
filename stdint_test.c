c3f173b0468deeb29210a5fd0fb04dad0f048dd1
DragonTalk <752963e64@tutanota.com>
2023-02-19 23:37:39 +0100

    testing like a professional :)

--- stdint_test.c
+++ stdint_test.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   printf(
-    "%d, %d, %d, %ld, %d, %d, %u, %lu, %d, %d, %d, %d, %d, %d, %d, %d\n",
+    "%d, %d, %d, %ld, %d, %d, %u, %lu, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",
     INT8_C(0x7FFFFFFFFFFFFFFF),
     INT16_C(0x7FFFFFFFFFFFFFFF),
     INT32_C(0x7FFFFFFFFFFFFFFF),
@@ -19,7 +19,15 @@ int main(void)
     sizeof (UINT8_C(0x7FFFFFFFFFFFFFFF)),
     sizeof (UINT16_C(0x7FFFFFFFFFFFFFFF)),
     sizeof (UINT32_C(0x7FFFFFFFFFFFFFFF)),
-    sizeof (UINT64_C(0x7FFFFFFFFFFFFFFF))
+    sizeof (UINT64_C(0x7FFFFFFFFFFFFFFF)),
+    sizeof (INT8_C(0x7)),
+    sizeof (INT16_C(0x7FF)),
+    sizeof (INT32_C(0x7FFF)),
+    sizeof (INT64_C(0x7FFFFFFF)),
+    sizeof (UINT8_C(0x7)),
+    sizeof (UINT16_C(0x7FF)),
+    sizeof (UINT32_C(0x7FFFF)),
+    sizeof (UINT64_C(0x7FFFFFFF))
   );
   return 0;
 }
