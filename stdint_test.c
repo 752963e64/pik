3724b18a6b641d5adae5b631bfc0b43a8c9c6917
DragonTalk <752963e64@tutanota.com>
2023-02-19 23:32:52 +0100

    Don't need casting... adds UINTn_C().

--- stdint_test.c
+++ stdint_test.c
@@ -3,19 +3,23 @@
 int main(void)
 {
   printf(
-    "%d, %d, %d, %ld, %d, %d, %u, %lu, %d, %d, %d, %d\n",
+    "%d, %d, %d, %ld, %d, %d, %u, %lu, %d, %d, %d, %d, %d, %d, %d, %d\n",
     INT8_C(0x7FFFFFFFFFFFFFFF),
     INT16_C(0x7FFFFFFFFFFFFFFF),
     INT32_C(0x7FFFFFFFFFFFFFFF),
     INT64_C(0x7FFFFFFFFFFFFFFF),
-    (unsigned)UINT8_C(0xFFFFFFFFFFFFFFFF),
-    (unsigned)UINT16_C(0xFFFFFFFFFFFFFFFF),
-    (unsigned)UINT32_C(0xFFFFFFFFFFFFFFFF),
-    (unsigned)UINT64_C(0xFFFFFFFFFFFFFFFF),
+    UINT8_C(0xFFFFFFFFFFFFFFFF),
+    UINT16_C(0xFFFFFFFFFFFFFFFF),
+    UINT32_C(0xFFFFFFFFFFFFFFFF),
+    UINT64_C(0xFFFFFFFFFFFFFFFF),
     sizeof (INT8_C(0x7FFFFFFFFFFFFFFF)),
     sizeof (INT16_C(0x7FFFFFFFFFFFFFFF)),
     sizeof (INT32_C(0x7FFFFFFFFFFFFFFF)),
-    sizeof (INT64_C(0x7FFFFFFFFFFFFFFF))
+    sizeof (INT64_C(0x7FFFFFFFFFFFFFFF)),
+    sizeof (UINT8_C(0x7FFFFFFFFFFFFFFF)),
+    sizeof (UINT16_C(0x7FFFFFFFFFFFFFFF)),
+    sizeof (UINT32_C(0x7FFFFFFFFFFFFFFF)),
+    sizeof (UINT64_C(0x7FFFFFFFFFFFFFFF))
   );
   return 0;
 }
